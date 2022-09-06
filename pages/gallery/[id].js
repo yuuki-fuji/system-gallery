b1980ffbfeb8b903c14442d3822752b89acb5ce6
fujimoto <yuukikun666@gmail.com>
2022-09-06 21:29:30 +0900

    done

---
+++ pages/gallery/[id].js
@@ -0,0 +1,33 @@
+import { client } from "../../libs/client";
+
+//SSG
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "gallery", contentId: id});
+
+  return {
+    props:{
+      gallery: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "gallery" });
+
+  const paths = data.contents.map((content) => `/gallery/${content.id}`);
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default function GalleryId({ gallery }) {
+  return (
+      <main>
+        <h1>{gallery.title}</h1>
+        <p>{gallery.publishedAt}</p>
+        <div dangerouslySetInnerHTML={{__html: `${gallery.body}`}}></div>
+      </main>
+  );
+}
